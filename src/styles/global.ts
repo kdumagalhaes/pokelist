0601c362a058259a2049bd15795615e10b39db13
Kadu <ce.magalhaespereira@gmail.com>
2023-01-23 13:45:22 -0300

    Add global styles

---
+++ src/styles/global.ts
@@ -0,0 +1,23 @@
+import styled, { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyles = createGlobalStyle`
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    list-style: none;
+}
+body {
+    font-family: 'Roboto', sans-serif;
+    background-color: #ccc;
+    color: #000;
+    -webkit-font-smoothing: antialiased;
+}
+body, input, textarea, button {
+    font-weight: 400;
+    font-size: 1rem;
+}
+button {
+    cursor: pointer;
+}
+`
