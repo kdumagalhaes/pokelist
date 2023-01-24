8457ebd600dbefa079339bd0075ee0dda5f281be
Kadu <ce.magalhaespereira@gmail.com>
2023-01-23 21:05:32 -0300

    Add react router dom config

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import React from 'react'
 import { ThemeProvider } from 'styled-components'
-import { Header } from './components/Header/Header'
-import { PokeContainer } from './components/PokeContainer/PokeContainer'
 import { PokeProvider } from './context/PokeContext'
 import { GlobalStyles } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './routes/Router'
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <PokeProvider>
-        <Header />
-        <PokeContainer />
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
       </PokeProvider>
       <GlobalStyles />
     </ThemeProvider>
