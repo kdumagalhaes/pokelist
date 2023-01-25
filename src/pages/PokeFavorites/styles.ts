5571679e313faf5a6d3f854b45ab266636a960e3
Kadu <ce.magalhaespereira@gmail.com>
2023-01-25 13:22:38 -0300

    Add empty favorites message && fix favorite feature and route

--- src/pages/PokeFavorites/styles.ts
+++ src/pages/PokeFavorites/styles.ts
@@ -22,3 +22,35 @@ export const Grid = styled.div`
     padding: 0 2rem;
   }
 `
+
+export const EmptyListMessage = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  .sad-pikachu {
+    width: 150px;
+    height: 150px;
+  }
+
+  .message {
+    color: ${({ theme }) => theme.darkBlue};
+    font-weight: 700;
+    font-size: 1.2rem;
+  }
+
+  .catch-pokemon-btn {
+    background-color: ${({ theme }) => theme.blue};
+    padding: 1rem 4rem;
+    border-radius: 5px;
+    border: none;
+    font-weight: 700;
+    color: ${({ theme }) => theme.yellow};
+    transition: background-color 0.3s;
+    margin-top: 2rem;
+
+    &:hover {
+      background-color: ${({ theme }) => theme.darkestBlue};
+    }
+  }
+`
